2052c386f12c54824fffac7c57392f46bc0c32cc
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2026-03-04 03:09:21 +0300

    convex bağlantısı widget klasörü ile sağlandı.

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { query, mutation } from "./_generated/server.js";
 
 export const getMany = query({
     args: {},
@@ -8,3 +9,35 @@ export const getMany = query({
         return users;
     },
 });
+
+export const add = mutation({
+    args: {
+      name: v.string(),
+    },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      console.log({ identity });
+      const orgId = identity.organizationId;
+  
+      if (!orgId) {
+        throw new Error("No organization id");
+      }
+  
+      const userId = await ctx.db.insert("users", {
+        name: args.name,
+      });
+  
+      return userId;
+    },
+  });
+  
+  declare module "convex/server" {
+    interface UserIdentity {
+      organizationId?: string;
+    }
+  }
