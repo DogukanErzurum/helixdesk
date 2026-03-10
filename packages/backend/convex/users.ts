dd35620eb041016a58964cfdb4c5262aa19303a4
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2026-03-10 04:30:51 +0300

    Projenin 4. modülü olan organizasyon modülü  tamamlandı.

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -2,27 +2,42 @@ import { v } from "convex/values";
 import { query, mutation } from "./_generated/server.js";
 
 export const getMany = query({
-    args: {},
-    handler: async (ctx) => {
-        const users = await ctx.db.query("users").collect();
-        return users;
-    },
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
 });
 
 export const add = mutation({
-    args: {},
-    handler: async (ctx) => {
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
 
-        const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
 
-        if (identity === null) {
-            throw new Error("Not authenticated");
-        }
+    console.log({ identity });
+    const orgId = identity.organizationId;
 
-        const userId = await ctx.db.insert("users", {
-            name: "Dogukan",
-        });
-        return userId;
-    },
+    if (!orgId) {
+      throw new Error("No organization id");
+    }
+
+    const userId = await ctx.db.insert("users", {
+      name: args.name,
+    });
+
+    return userId;
+  },
 });
 
+// This has been added to the jwt template from clerk
+declare module "convex/server" {
+  interface UserIdentity {
+    organizationId?: string;
+  }
+}
